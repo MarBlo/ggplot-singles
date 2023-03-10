dc0386ef4eebc83340e9273dee7c5f7afa41baeb
MarBlo <MarBlo@users.noreply.github.com>
2023-03-10 17:30:20 +0100

    cleaned functions, added function description

--- 2023-03-06_Triangles.r
+++ 2023-03-06_Triangles.r
@@ -1,20 +1,57 @@
 library(ggplot2)
 library(dplyr)
 library(tibble)
+library(cowplot)
+library(gridExtra)
 
 source(here::here("2023-03-06_helper-Triangles.r"))
 source(here::here("2023-03-06_worker-Triangles.r"))
 
 param <- list(
+  #' makes 24 plots each with several triangles
+  #' angle between triangles
   degrees = 10,
-  rotation = 10,
-  x_point = c(3:10),
-  y_point = c(2:5),
-  mit_schwerpunkt = TRUE,
+  #' coordinates of points b and c
+  x_point_b = c(4, 9),
+  y_point_b = c(1, 6),
+  x_point_c = c(1, 5),
+  y_point_c = c(1, 5),
+  #' should schwerpunkt be plotted; if set size
+  mit_schwerpunkt = FALSE,
   size = 2,
+  #' should pathline be made, if set linewidth
   mit_pathline = TRUE,
-  linewidth = .5
+  linewidth = .2,
+  #' should own defined colors be used
+  own_col = TRUE,
+  color_own = my_cols,
+  #' settings for PrettyColors
+  color_pal = "Blues",
+  color_back = "darkgray"
 )
 
 
-make_triangles(param)
+#' MAIN FUNCTION
+#'
+#' @description
+#'  * creates 24 plots in facets
+#'  * each plots has randomly chosen number of triangles
+#' @param param see above
+#' @details
+#'  * first call is to `make_triangles`
+#'
+
+grid_24_triangles(param)
+
+
+#| Experimentell ----
+#| F, K
+#| dgrees = 10, rotation = 10
+# s_p <- df |>
+#   filter(rw == "F")
+# print(plot_single(s_p, param))
+
+
+# saveRDS(s_p, file = here::here("schöner_DF.rds"))
+# # saveRDS(s_p, file = here::here("schöner_DF_2"))
+# f <- s_p[1:3, 1:3]
--- 2023-03-06_worker-Triangles.r
+++ 2023-03-06_worker-Triangles.r
@@ -1,4 +1,49 @@
-#| data_frame for every new triangle ---
+#| Colors ----
+library(PrettyCols)
+my_cols <- c(
+  "#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84",
+  "#fc8d59", "#ef6548", "#d7301f", "#b30000",
+  "#7f0000", "#fff7ec", "#fee8c8", "#fdd49e",
+  "#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84",
+  "#fc8d59", "#ef6548", "#d7301f", "#b30000",
+  "#7f0000", "#fff7ec", "#fee8c8", "#fdd49e",
+  "#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84",
+  "#fc8d59", "#ef6548", "#d7301f", "#b30000"
+)
+
+my_greens <- c(
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc",
+  "#22577a", "#38a3a5", "#57cc99", "#80ed99", "#c7f9cc"
+)
+
+many_greens <- c(
+  "#007f5f", "#2b9348", "#55a630", "#80b918", "#aacc00",
+  "#bfd200", "#d4d700", "#dddf00", "#eeef20", "#ffff3f",
+  "#007f5f", "#2b9348", "#55a630", "#80b918", "#aacc00",
+  "#bfd200", "#d4d700", "#dddf00", "#eeef20", "#ffff3f",
+  "#007f5f", "#2b9348", "#55a630", "#80b918", "#aacc00",
+  "#bfd200", "#d4d700", "#dddf00", "#eeef20", "#ffff3f",
+  "#007f5f", "#2b9348", "#55a630", "#80b918", "#aacc00",
+  "#bfd200", "#d4d700", "#dddf00", "#eeef20", "#ffff3f"
+)
+
+
+
+#| Worker Functions  ----
+
+#' make_dreieck  erstellt DF für Dreiecke
+#'
+#' @param origin of triangle
+#' @param b second point
+#' @param c third point
+#' @return tibble with coordinates
+#'
 
 make_dreieck <- function(origin = c(0, 0), b, c, label) {
   df <- tibble(
@@ -14,8 +59,13 @@ make_dreieck <- function(origin = c(0, 0), b, c, label) {
 }
 
 
-## | Dreieck drehen          ----
-## | Drehen um den Nullpunkt ----
+#' Turns Triangle around origin
+#'
+#' @param data_f DF with coordinates
+#' @param winkel Truning angle
+#' @param label bb
+#' @return  new DF with coordinates
+#'
 
 drehen_dreieck <- function(data_f, winkel = 10, label = "") {
   dreh_winkel <- winkel # in degree
@@ -54,7 +104,13 @@ drehen_dreieck <- function(data_f, winkel = 10, label = "") {
   return(df)
 }
 
-#| add Schwerpunkt
+#' schwerpunkt
+#' calculates schwerpunkt Triangle
+#'
+#' @param data_f coordinates of Triangle
+#' @return DF with coordinates of schwerpunkt
+#'
+
 schwerpunkt <- function(data_f) {
   df <- data_f |>
     group_by(label) |>
@@ -67,7 +123,13 @@ schwerpunkt <- function(data_f) {
   return(df)
 }
 
-#| add Seitenhalbierende
+#' seitenhalbierende
+#' calculats coordinates for seitenhalbierende
+#'
+#' @param data_f coordinates of Triangle
+#' @return DF with coordinates of seitenhalbierende
+#'
+
 seitenhalbierende <- function(data_f) {
   #| zum plotten der seitenhalbierende mit geom_point
   #| werden die Variablen x_2 und y_2 genommen
@@ -86,19 +148,54 @@ seitenhalbierende <- function(data_f) {
   return(df)
 }
 
-
-#| Make Triangles ----
+#' make_triangles
+#'
+#' @description
+#'  * random samples for making corner points of triangle
+#'  * checks if coordinates of point b is the sames as point c
+#'  * if so, continues sampling
+#'  * in second part calls `make_dreieck` for makeing first triangle
+#'  * calls then `drehen_dreieck` as many times as defined in
+#'  * main function `grid_triangles()` and binds DF together
+#'
+#' @param param from main call function
+#' @return DF ready for plotting
+#' @details
+#'  * calls make_dreieck
+#'  * calls drehen_dreieck
+#'  * get called by main function `grid_24_triangles()`
+#'  * number of triangles per plot is randomly sampled => `rot`
+#'
 
 make_triangles <- function(param) {
-  all_x_points <- param$x_point
-  all_y_points <- param$y_point
-  b <- c(sample_points(all_x_points), sample_points(all_y_points))
-  c <- c(sample_points(all_x_points), sample_points(all_y_points))
+  all_x_points_b <- param$x_point_b
+  all_y_points_b <- param$y_point_b
+  all_x_points_c <- param$x_point_c
+  all_y_points_c <- param$y_point_c
+  b <- c(sample_points(all_x_points_b), sample_points(all_y_points_b))
+  c <- c(sample_points(all_x_points_c), sample_points(all_y_points_c))
+  b_equal_c <- b[1] == c[1] & b[2] == c[2]
+  print(b_equal_c)
+  print(b)
+  print(c)
+  count <- 1
+  while (b_equal_c) {
+    count <- count + 1
+    print("--------")
+    print(count)
+    print("inside")
+    b <- c(sample_points(all_x_points_b), sample_points(all_y_points_b))
+    c <- c(sample_points(all_x_points_c), sample_points(all_y_points_c))
+    b_equal_c <- b[1] == c[1] & b[2] == c[2]
+    print(b_equal_c)
+    print("--------")
+  }
 
   f <- make_dreieck(b = b, c = c, label = "1")
   tt <- NULL
   f_0 <- f
-  for (i in seq(, param$rotation)) {
+  rot <- sample(1:20, 1)
+  for (i in seq(rot)) {
     lab <- i + 1
     tt <- drehen_dreieck(
       f_0,
@@ -107,26 +204,95 @@ make_triangles <- function(param) {
     f <- bind_rows(f, tt[4:6, ])
   }
 
-  plot_f(f, param)
+  return(f)
 }
 
-#| Plot ---
+#' plot_f
+#'
+#' @description
+#'  * plots as many plots as defined in main function `grid_triangle()`
+#'  * (default = 24) and makes facet
+#'
+#' @param param
+#' @returns ggplot object
+#' @details
+#'  * get called by main function `grid_triangle()`
+#'  * creates some `geom_`s if boolean is set TRUE in @param
+#'  * PrettyColors is an option
+#'  * calls `mit_schwerpunkt()` if TRUE in @param
+#'  * calls `mit_pathline()` if TRUE in @param
+#'
+
 plot_f <- function(f, param) {
   if (param$mit_schwerpunkt) {
     sp <- schwerpunkt(f)
-    print(sp)
   }
   if (param$mit_pathline) {
     ff_path <- f |>
-      group_by(label) |>
+      group_by(label, rw) |>
       group_modify(~ add_row(
         x = 0,
         y = 0,
         .x
-      ))
+      )) |>
+      mutate(rw = ifelse(is.na(rw), lag(rw), rw))
   }
 
   drei_e <- ggplot(f) +
+    geom_polygon(aes(x = x, y = y, fill = label), alpha = .9) +
+    { # nolint: brace_linter.
+      if (param$mit_pathline) {
+        geom_path(data = ff_path, aes(
+          x = x, y = y, group = label
+        ), color = "white", linewidth = param$linewidth)
+      }
+    } +
+    { # nolint: brace_linter.
+      if (param$mit_schwerpunkt) {
+        geom_point(
+          data = sp, aes(x = mp_x, y = mp_y, color = label),
+          size = param$size
+        )
+      }
+    } +
+    scale_fill_pretty_d(param$color_pal) +
+    { # nolint: brace_linter.
+      if (param$own_col) {
+        scale_fill_manual(values = param$color_own)
+      }
+    } +
+    facet_wrap(~rw, ncol = 6) +
+    coord_equal(xlim = c(-9, 9), ylim = c(-9, 9)) +
+    theme_void() +
+    guides(color = FALSE, fill = FALSE) +
+    theme(
+      plot.background = element_blank(),
+      panel.background = element_rect(
+        fill = param$color_back, color = NA
+      ),
+      plot.margin = margin(1, 1, 1, 1, unit = "cm"),
+      strip.text = element_blank(),
+      panel.spacing = unit(.1, "lines")
+    )
+
+  return(drei_e)
+}
+
+plot_single <- function(data_f, param) {
+  if (param$mit_schwerpunkt) {
+    sp <- schwerpunkt(f)
+  }
+  if (param$mit_pathline) {
+    ff_path <- data_f |>
+      group_by(label, rw) |>
+      group_modify(~ add_row(
+        x = 0,
+        y = 0,
+        .x
+      )) |>
+      mutate(rw = ifelse(is.na(rw), lag(rw), rw))
+  }
+  single_plot <- ggplot(data_f) +
     geom_polygon(aes(x = x, y = y, fill = label), alpha = .2) +
     { # nolint: brace_linter.
       if (param$mit_pathline) {
@@ -143,10 +309,97 @@ plot_f <- function(f, param) {
         )
       }
     } +
-    coord_equal(xlim = c(-10, 10), ylim = c(-10, 10)) +
+    # scale_fill_pretty_d(param$color_pal) +
+    scale_fill_manual(values = my_cols) +
+    facet_wrap(~rw, ncol = 6) +
+    coord_equal(xlim = c(-9, 9), ylim = c(-9, 9)) +
     theme_void() +
     guides(color = FALSE, fill = FALSE) +
-    theme(plot.background = element_rect(fill = "white"))
-
-  return(drei_e)
+    theme(
+      plot.background = element_blank(),
+      panel.background = element_rect(
+        fill = param$color_back, color = NA
+      ),
+      plot.margin = margin(1, 1, 1, 1, unit = "cm"),
+      strip.text = element_blank(),
+      panel.spacing = unit(.1, "lines")
+    )
 }
+
+#| End functions ----
+
+#' grid_24_triangles
+#'
+#' @description
+#'  * creates a list of 24 DF, each contains one set of
+#'      triangles
+#'  * This list is used to make a DF
+#' @param param
+#' @return ggplot with 24 facets
+#' @details
+#'  * calls `make_triangles`
+#'    * which calls `make_dreieck`
+#'    * which calls `drehen_dreieck`
+#'
+
+grid_24_triangles <- function(param) {
+  plot_lst <- vector("list", length = 8)
+
+  for (i in 1:24) {
+    g <- make_triangles(param) |>
+      mutate(rw = LETTERS[i])
+    plot_lst[[i]] <- g
+  }
+
+  df <- bind_rows(plot_lst)
+  print(plot_f(df, param))
+}
+
+#| Experimentell ----
+
+# f <- make_dreieck(b = b, c = c, label = "1")
+
+# s_p <- readRDS(here::here("schöner_DF.rds"))
+# f <- s_p[1:3, 1:3]
+# tt <- NULL
+# f_0 <- f
+# # rot <- sample(1:20, 1)
+# rot <- 28
+# for (i in seq(rot)) {
+#   lab <- i + 1
+#   tt <- drehen_dreieck(
+#     f_0,
+#     winkel = i * 10, label = as.character(lab)
+#   )
+#   f <- bind_rows(f, tt[4:6, ])
+# }
+# ff <- f |>
+#   group_by(label) |>
+#   group_modify(~ add_row(
+#     x = 0,
+#     y = 0,
+#     .x
+#   )) |>
+#   mutate(rw = LETTERS[as.numeric(label)])
+
+
+# ggplot(ff) +
+#   geom_polygon(aes(x = x, y = y, fill = label), alpha = .8) +
+#   geom_path(aes(
+#     x = x, y = y, group = label
+#   ), color = "white", linewidth = 1.2) +
+#   # scale_fill_pretty_d(param$color_pal) +
+#   scale_fill_manual(values = many_greens) +
+#   # facet_wrap(~rw, ncol = 6) +
+#   coord_equal(xlim = c(-9, 9), ylim = c(-9, 9)) +
+#   theme_void() +
+#   guides(color = FALSE, fill = FALSE) +
+#   theme(
+#     plot.background = element_blank(),
+#     panel.background = element_rect(
+#       fill = "black", color = NA
+#     ),
+#     # plot.margin = margin(1, 1, 1, 1, unit = "cm"),
+#     strip.text = element_blank(),
+#     panel.spacing = unit(.1, "lines")
+#   )
